ad88a6fe66b0d2d586f1de0b0430448bac48ccb6
asimmon3 <asimmon3@nd.edu>
2021-03-08 14:55:18 -0500

    made AboutYou page work

--- personal_portfolio-project/aboutyou/views.py
+++ personal_portfolio-project/aboutyou/views.py
@@ -6,6 +6,6 @@ def about_you(request):
     aboutyous = AboutYou.objects.all()
     return render(request, 'aboutyou/about_you.html', {'aboutyous':aboutyous})
 
-def detail(request, aboutme_id):
+def detail(request, aboutyou_id):
     story = get_object_or_404(AboutYou, pk = aboutyou_id)
     return render(request, 'aboutyou/detail.html', {'story':story})
